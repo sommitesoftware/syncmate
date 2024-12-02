a9e5674021f946c072a727bc7eb7d4f11e31cc0c
Sommité <dinand.veldman@sommite.nl>
2024-12-02 15:37:29 +0100

    Initial Setup of SyncMate Windows

---
+++ 0.Windows/SyncMate Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Skins;
+using DevExpress.UserSkins;
+using SyncMate_Client.Forms.Special;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SyncMate_Client
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
